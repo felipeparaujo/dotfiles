4722449cfed1092483f6c9fbc39ccdd8af7df824
Felipe Araujo <felipe.peixoto.araujo@gmail.com>
2017-01-18 16:33:19 +0000

    safer shit

--- createSoftLinks.sh
+++ createSoftLinks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir "$HOME/.env"
-mv $PWD/* "$HOME/.env"
+cp $PWD/* "$HOME/.env"
 ln -s "$HOME/.env/zsh/.zshrc" "$HOME/.zshrc"
 ln -s "$HOME/.env/vim/.vim" "$HOME/.vim"
 ln -s "$HOME/.env/vim/.vimrc" "$HOME/.vimrc"
