674fcdfcd563a69387e58a632287e5b95b4a311f
Felipe Araujo <felipe.araujo@immo.capital>
2019-10-28 13:25:17 +0000

    Fix errors in setup script

--- setup.sh
+++ setup.sh
@@ -3,27 +3,47 @@ set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-setup_zsh
-setup_vim
-
 setup_zsh() {
   cp -f "$SCRIPTPATH/zsh/.zshenv" "$HOME/.zshenv"
-  exec "$SHELL"
+  source "$HOME/.zshenv"
 
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  echo $ZDOTDIR
 
-  cp -f "$SCRIPTPATH/zsh/.zshrc" "${ZDOTDIR:-$HOME}/.zprezto/runcoms/"
-  cp -f "$SCRIPTPATH/zsh/.zpreztorc" "${ZDOTDIR:-$HOME}/.zprezto/runcoms/"
+  cp -f "$SCRIPTPATH/zsh/zshrc" "${ZDOTDIR:-$HOME}/.zprezto/runcoms/"
+  cp -f "$SCRIPTPATH/zsh/zpreztorc" "${ZDOTDIR:-$HOME}/.zprezto/runcoms/"
   cp -f "$SCRIPTPATH/zsh/.p10k.zsh" "${ZDOTDIR:-$HOME}/"
 
   setopt EXTENDED_GLOB
   for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
+  
+  source "${ZDOTDIR:-$HOME}/.zshrc"
 }
 
 setup_vim() {
   git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.config/nvim/bundle/Vundle.vim"
   cp "$SCRIPTPATH/nvim/init.vim" "$HOME/.config/nvim/"
-  vim +PluginInstall +qall
+  nvim +PluginInstall +qall
 }
+
+if ! type git > /dev/null; then
+  echo "Please install git to continue"
+  exit 1
+fi
+
+if ! type pyenv > /dev/null; then
+  echo "Please install pyenv to continue"
+  exit 1
+fi
+
+if ! type nvim > /dev/null; then
+  echo "Please install NeoVim to continue"
+  exit 1
+fi
+
+setup_zsh
+setup_vim
+
+exec "$SHELL"
