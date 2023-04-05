29019d11851c8a9368ada5a8b70aacc3f7b37483
Felipe Araujo <felipe.araujo@sensat.co.uk>
2023-04-05 15:31:19 +0100

    Simplify astronvim config

--- .config/astronvim/init.lua
+++ .config/astronvim/init.lua
@@ -1,85 +1,48 @@
 return {
-  -- Configure AstroNvim updates
-  updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
-    show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
-    remotes = { -- easily add new remotes to track
-      --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
-      --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
-      --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
-    },
+  opt = {
+    relativenumber = false
   },
 
-  -- Set colorscheme to use
   colorscheme = "catppuccin",
 
-  -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
-  },
-
-  lsp = {
-    -- customize lsp formatting options
-    formatting = {
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
-      },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- "sumneko_lua",
-      },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
+  plugins = {
+    {
+      "catppuccin/nvim",
+      config = function()
+        require("catppuccin").setup {
+          color_overrides = {
+            mocha = {
+              base = "#030303",
+              mantle = "#020202",
+              crust = "#010101"
+            }
+          }
+        }
+      end,
     },
-    -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
+    {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require("astronvim.utils.status")
+        opts.statusline = { -- statusline
+          hl = { fg = "fg", bg = "bg" },
+          status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+          status.component.git_branch(),
+          status.component.file_info { filetype = {}, filename = false, file_modified = false },
+          status.component.git_diff(),
+          status.component.diagnostics(),
+          status.component.fill(),
+          status.component.cmd_info(),
+          status.component.fill(),
+          status.component.lsp(),
+          status.component.treesitter(),
+          status.component.nav(),
+          -- remove the 2nd mode indicator on the right
+        }
+
+        -- return the final configuration table
+        return opts
+      end,
     },
-  },
-
-  -- Configure require("lazy").setup() options
-  lazy = {
-    defaults = { lazy = true },
-    performance = {
-      rtp = {
-        -- customize default disabled vim plugins
-        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
-      },
-    },
-  },
-
-  -- This function is run last and is a good place to configuring
-  -- augroups/autocommands and custom filetypes also this just pure lua so
-  -- anything that doesn't fit in the normal config locations above can go here
-  polish = function()
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
-  end,
-
+  }
 }
